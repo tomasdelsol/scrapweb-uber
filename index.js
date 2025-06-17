c5b4475376201489cf2ab763ea5417d6804071ca
Tomas <tomasdelsol02@gmail.com>
2025-06-17 11:58:24 -0300

    avance 1

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 import puppeteer from "puppeteer";
+import fs from "fs/promises";
+
+async function handleDynamicUberPage() {
+
+    const browser = await puppeteer.launch({
+        headless: false,
+        executablePath: '/usr/bin/google-chrome',
+        slowMo: 300,
+    })
+
+    const page = await browser.newPage();
+
+    await page.goto("https://www.uber.com/global/en/eligible-vehicles/?city=bahia-blanca");
+
+    const result = await page.evaluate(() => {
+        const carsUber = document.querySelectorAll('div [data-testid = "accordion-header"]');
+        const dataCarsUber = [...carsUber].map((accordion-header) => {
+            const carMark = accordion-header.querySelector() 
+        });
+        return dataCarsUber;
+    });
+    console.log(result);
+
+    await browser.close();
+}
+
+handleDynamicUberPage();

2624c7463293f8256ecac2049483685a0979b8df
Tomas <tomasdelsol02@gmail.com>
2025-06-19 12:05:41 -0300

    avance 2

--- index.js
+++ index.js
@@ -14,12 +14,47 @@ async function handleDynamicUberPage() {
     await page.goto("https://www.uber.com/global/en/eligible-vehicles/?city=bahia-blanca");
 
     const result = await page.evaluate(() => {
-        const carsUber = document.querySelectorAll('div [data-testid = "accordion-header"]');
-        const dataCarsUber = [...carsUber].map((accordion-header) => {
-            const carMark = accordion-header.querySelector() 
+
+        /*function parsePlansGrouped(text) {
+        const entries = text.split("/").map((e) => e.trim());
+        const grouped = [];
+
+        entries.forEach((entry) => {
+            const match = entry.match(/(\d{4})\s*\((.*?)\)/);
+            if (!match) return;
+
+            const modelYear = match[1];
+            const planNames = match[2].split(",").map((s) => s.trim());
+
+            grouped.push({ modelYear, planNames });
         });
+
+        return grouped; 
+
+        } */
+
+        const carsUber = document.querySelectorAll('ul[data-testid="wcb3-accordion-component.root"] > li');
+        const dataCarsUber = [];
+
+            [...carsUber].forEach((carInfo) => {
+                const markerName = carInfo.querySelector('[data-testid="accordion-header"]')?.innerText.trim() || '';;
+                
+                const modelTags = carInfo.querySelector('b');
+                modelTags.forEach((b) => {
+                    const modelName = b.innerText.trim();
+                    const dataModelTags = b.parentElement?.innerText.trim() || '';
+                    const detailInfo = dataModelTags.replace(modelName, '').trim();
+
+                    const pĺans = parsePlansGrouped(detailInfo);
+
+                    dataCarsUber.push({ markerName, modelName, plans });
+                })
+            });
+
         return dataCarsUber;
+
     });
+
     console.log(result);
 
     await browser.close();
